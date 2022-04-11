0bda23f9fdedf3b1926fc9550190b900503411ab
thanhntphys <thanhntphys@gmail.com>
2022-04-11 10:15:40 +0700

    update project 2

--- src/server.ts
+++ src/server.ts
@@ -31,7 +31,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   //! END @TODO1
 
-  app.get("/filteredimage", async(req, res) => {
+  app.get("/filteredimage", async(req: express.Request, res: express.Response) => {
     let imgUrl:string = req.query.image_url
 
     if (!imgUrl) return res.status(400).send('image_url is required')
